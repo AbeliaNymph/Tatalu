458f7232e0fc7d56a70c9267545c3c70acb82c4f
Abelia Nymph <Abelianymph@outlook.com>
2021-04-21 09:11:42 +0800

    modify Program.fs, Model.fs;add db file

---
+++ Tatalu/Database.fs
@@ -0,0 +1,106 @@
+﻿module Database
+
+open Microsoft.Data.Sqlite
+
+let connection_string = "Data Source=database/tatalu.db"
+
+type Sqlite private() = 
+    static let connection = new SqliteConnection(connection_string)
+    
+    static let instance = Sqlite()
+
+    static member Instance = instance
+
+    member self.Connection = connection
+
+
+let select (sql: string) (inserter: (SqliteCommand -> Unit) option) (parser: SqliteDataReader -> 'a) = 
+    let sqlite = Sqlite.Instance
+    
+    use connection = sqlite.Connection
+        
+    connection.Open()
+    
+    use command = connection.CreateCommand()
+    
+    command.CommandText <- sql
+    
+    match inserter with
+    | Some fn -> fn command
+    | None -> ()
+
+    use reader = command.ExecuteReader()
+    
+    let list = List.empty
+    
+    let rec read (reader: SqliteDataReader) list = 
+        match reader.Read() with
+        | true -> read reader ((parser reader)::list)
+        | false -> list
+    
+    read reader list
+
+
+let select_all_cards_from_card_by_list_id id = 
+    let sql = 
+        """
+        select
+            card.id,
+            card.title
+        from
+            card
+        where
+            list_id = $id
+        """
+
+    let inserter (command: SqliteCommand) = 
+        command.Parameters.AddWithValue("$id", id) |> ignore
+
+    let parser (reader: SqliteDataReader) = 
+        (reader.GetInt32(0), reader.GetString(1))
+
+    select sql (Some inserter) parser
+    
+
+
+let select_all_kanbans_from_Kanban _ = 
+
+    let sql = 
+        """
+            select
+                *
+            from
+                Kanban
+        """
+
+    let inserter = None
+
+    let parser (reader: SqliteDataReader) = 
+        (reader.GetInt32(0), reader.GetString(1))
+
+    select sql inserter parser
+
+
+let select_all_lists_from_list_by_kanban_id id = 
+    
+    let sql =
+        """
+            select
+                list.id,
+                list.title
+            from
+                list
+            where
+                kanban_id = $id
+        """
+
+
+    let inserter = Some (fun (command: SqliteCommand) -> command.Parameters.AddWithValue("$id", id) |> ignore)
+
+    let parser (reader: SqliteDataReader) = 
+        (reader.GetInt32(0), reader.GetString(1))
+        
+    select sql inserter parser
+
+    
+        
--- Tatalu/Program.fs
+++ Tatalu/Program.fs
@@ -1,8 +1,119 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open Microsoft.Data.Sqlite
 
-open System
+let help _ =
+    """Usage: Tatalu <command> [<args>]
+        
+    """
+    |> printfn "%s"
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+
+    match argv.Length with
+    | 2 ->
+        match argv.[0] with
+        | "kanban" ->
+            match argv.[1] with
+            | "list" ->
+                Database.select_all_kanbans_from_Kanban()
+                |> List.map (fun (id, title) -> Model.Kanban.create id title None)
+                |> List.map (fun kanban -> 
+                    {
+                        kanban with lists = 
+                            Database.select_all_lists_from_list_by_kanban_id kanban.id
+                            |> List.map (fun (list_id, list_title) -> Model.List.create list_id list_title None)
+                            |> List.map (fun list -> 
+                                {
+                                    list with cards = 
+                                        Database.select_all_cards_from_card_by_list_id list.id
+                                        |> List.map (fun (card_id, card_title) -> Model.Card.create card_id card_title)
+                                        |> List.map (fun card -> card.title,card)
+                                        |> Seq.ofList
+                                        |> Map
+                                        |> Some
+                                }
+                            )
+                            |> List.map (fun list -> list.title, list)
+                            |> Seq.ofList
+                            |> Map
+                            |> Some
+                    }
+                )
+                |> printfn "%A"
+                
+            | _ -> eprintfn "command not found."
+
+        | _ -> eprintfn "command not found."
+    | _ -> help()
+
+
+    //use connection = new SqliteConnection("Data Source=database/tatalu.db")
+    
+    //connection.Open()
+
+    //use command = connection.CreateCommand()
+
+    //command.CommandText <- 
+    //    """
+    //        select 
+    //            *
+    //        from
+    //            Kanban
+    //        where
+    //            id = $id
+    //    """
+    //command.Parameters.AddWithValue("$id", 1) |> ignore
+
+    //use reader = command.ExecuteReader()
+
+    //while reader.Read() do
+    //    let id = reader.GetInt32(0)
+    //    let title = reader.GetString(1)
+
+    //    printfn "%A: %A" id title
+
+    
+    
+    //let manager = 
+    //    kanbans
+    //    |> List.map (
+
+    //        fun (kanban_id, kanban_title) ->
+
+    //            let list_seq = 
+    //                Database.select_all_lists_from_list_by_kanban_id kanban_id
+    //                |> List.map (
+
+    //                    fun (list_id, list_title) ->
+
+    //                        let card_seq = 
+    //                            Database.select_all_cards_from_card_by_list_id list_id
+    //                            |> List.map (
+
+    //                                fun (card_id, card_title) ->
+    //                                    (card_title, Model.Card.create card_id card_title)
+    //                            )
+    //                            |> List.toSeq
+
+    //                        (list_title, Model.List.create list_id list_title card_seq)
+                    
+    //                )
+    //                |> List.toSeq
+
+    //            (kanban_title, Model.Kanban.create kanban_id kanban_title list_seq)
+            
+    //    )
+    //    |> Model.Manager.create
+
+
+    //let mutable current_kanban = Model.Kanban.empty ()
+    //match manager.kanbans.TryFind "test kanban" with
+    //| Some k -> current_kanban <- k
+    //| None -> eprintfn "Kanban not found"
+    //printfn "%A" manager
+
+
+
+    0
